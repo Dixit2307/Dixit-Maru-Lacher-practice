3754ac7f221f361b79973297c1f816436fe73c18
Dixit2307 <marudixit18@gmail.com>
2026-05-13 15:31:47 +0530

    Day 2 13/5/26

---
+++ day 2.py
@@ -0,0 +1,34 @@
+
+print("hello,Dixit")
+a=20
+b=34
+sum=a+b
+
+print(sum)
+
+Name=input("What is your name?")
+
+print("Welcome to python programing name")
+
+name="dixit"
+print(type(name))
+
+#simple calculator
+
+a=int(input("Enter First no:"))
+b=int(input(" Enter second no:"))
+
+print("Add:",a+b)
+print("substraction:",a-b)
+print("multiplay:",a*b)
+print("division:",a/b)
+
+#silly pizza maker
+
+Base=input("choose your base(thik/soft):")
+toppings=input("choose your toppings( chees/Mashrum/tomato/cacumber")
+print("Here is your silly pizza")
+
+git remote add origin https://github.com/Dixit2307/Dixit-Maru-Lacher-practice.git
+git branch -M main
+git push -u origin main
