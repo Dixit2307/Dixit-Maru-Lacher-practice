11d66a64e11cce8a334962b129657141d55b8171
Dixit2307 <marudixit18@gmail.com>
2026-05-15 14:41:20 +0530

    15/5/26 L.P

---
+++ L.P 15 may.py
@@ -0,0 +1,67 @@
+fs=frozenset('Hello Python')
+print(fs)
+
+cities=frozenset(["surat","Ahemdabad","Vadodara","Pune","Mubai"])
+print(cities)
+
+# number have four types of datatypes(int,flot,complex, long)
+
+int_num=103
+flot_num=23.07
+
+print(int_num)
+print(flot_num)
+
+# list datatype
+
+list=[234,"data",37.8,"G"]
+print(list)
+
+list1=["python","introduction"]
+print(list1)
+
+print(list[0:6])
+print(list[0:2])
+print(list1*2)
+print(list+list1)
+
+# Dictionary Datatype
+dic={'name':'Dixit','Age':'17'}
+print(dic)
+
+print(dic ["name"])
+print(dic.values())
+print(dic.keys())
+
+tuple=(2026,"RW","right")
+print(tuple)
+print(tuple[0])
+
+tuple1=("Wight")
+print(tuple1)
+print(tuple+tuple1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
