d49a8d279f77e2a5abb944bf5640953a68b05935
Dixit2307 <marudixit18@gmail.com>
2026-05-18 14:58:46 +0530

    18 may 2026

---
+++ 18 May LP.py
@@ -0,0 +1,138 @@
+print("Hello World")
+
+# Operatiors
+
+# Arithmatic operator
+
+x=23
+y=56
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+
+# comparison Operator
+
+x=100
+y=120
+
+print(x==y)
+print(x!=y)
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+
+# assignment
+
+x=20
+y=18
+x=y
+print(x)
+
+x+=y
+print(x)
+
+x-=y
+print(x)
+
+x*=y
+print(x)
+print(y)
+
+y/=x
+print(x)
+print(y)
+
+x**=y
+print(x)
+
+x//=y
+print(x)
+
+# Logical
+
+#chake if the number is even or odd
+
+num=int(input("Enter a number"))
+
+if num%2==0:
+ print(f"{num}is an even number")
+else:
+ print(f"{num} is an odd number")
+
+#Find the minimum of Two number
+
+ num1=int(input("Enter first number"))
+ num2=int(input("Enter second number"))
+
+ if num1<num2:
+     print(f"The minimum number is{num1}")
+ else:
+     print(f"The minimum number is{num2}")
+
+#Find the Largest number of three number
+
+num1=int(input("Enter first number "))
+num2=int(input("Enter second number"))
+num3=int(input("Enter thiresd number"))
+
+if num1>=num2 and num1>=num3:
+    print(f"The largerst number is{num1}")
+elif num2>=num1 and num2>=num3:
+    print(f"The largest number is{num2}")
+else:
+    print(f"The largest number is{num3}")
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                   
+
+
+
+
+
+
+
+
