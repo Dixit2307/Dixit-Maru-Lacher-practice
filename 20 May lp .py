d2aba02c794ac8993a43fe6ea1cb3bb9f1b899f3
Dixit2307 <marudixit18@gmail.com>
2026-05-20 14:33:10 +0530

    Add files via upload

---
+++ 20 May lp .py
@@ -0,0 +1,75 @@
+# lecher practice 20 May
+
+x=[1,2,3]
+y=x
+z=[1,2,3]
+
+print(x is y)
+print(x is z)
+print(x is not z )
+
+#Membership Operator
+
+number=[1,2,3,4,5]
+print(2 in number)
+print(6 in number)
+print(6 not in number)
+
+#string
+
+text="welcome"
+print("py"in text)
+
+#Bitwise AND
+x=7
+y=9
+print(x & y)
+
+#Bitwise XOR
+
+x=7
+y=9
+print(x^y)
+
+#python conterol Flow
+
+# if, if...else, if....elif......else
+
+age=18
+if age>=18:
+    print("you are eligible for voting:")
+
+number=8
+if number%2==0:
+    print("Even")
+else:
+    print("odd")
+
+marks=70
+if marks>=90:
+   print("Grade A")
+elif marks>=75:
+   print("Grade B")
+elif marks>=50:
+    print("Grade c")
+else:
+    print("Fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
