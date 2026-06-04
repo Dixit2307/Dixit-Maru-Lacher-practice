399d20c95c4b6b2148c9029116b0f5038e2ba519
Dixit2307 <marudixit18@gmail.com>
2026-06-04 14:52:30 +0530

    Add files via upload

---
+++ 04-05-26/4 jun LP.py
@@ -0,0 +1,319 @@
+#Abstraction in OOP
+'''
+#1. abc(Absract Base Class)
+
+from abc import ABC,abstractmethod
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog Boww Boww.")
+
+d=Dog()
+d.sound()
+'''
+# Abstract Class and Method
+
+from abc import ABC,abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound (self):
+        pass
+    def sleep(self):
+        print("Animal is sleeping")
+#child calss
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog boww boww")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat Meow meow...")
+
+d=Dog()
+d.sound()
+d.sleep()
+
+c=Cat()
+c.sound()
+c.sleep()
+
+#Lab Work
+# Abstract class shape with area()
+
+from abc import ABC,abstractmethod
+class shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(shape):
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(shape):
+
+    def __init__(self,radius):
+        self.radius=radius
+
+    def area(self):
+        return 3.14*self.radius*self.radius
+
+
+r=Rectangle(9,4)
+c=Circle(11)
+
+print("Rectangle Area:",r.area())
+print("Circle Area:",c.area())
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
