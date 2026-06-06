74bef9ad029e99af9ed019ea54c82a540010c186
Dixit2307 <marudixit18@gmail.com>
2026-06-06 14:47:27 +0530

    Add files via upload

---
+++ 05-06-26/file.py
@@ -0,0 +1,19 @@
+#
+
+file=open("sample.txt", "w")
+file.write("Python is a versatile programming language.")
+file.close()
+
+#
+
+file=open("sample.txt", "r") 
+contend=file.read()
+file.close()
+print(contend)
+
+file=open("sample.txt", "w") 
+file.write("Learning file handling in Python is fun!")
+file.close
+print("Content overwritten.")
+
+#
